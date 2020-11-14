69c892665beed8ceab79be882343463ff42a14c2
AlbertoRG97 <alberto.rgmb@gmail.com>
2020-11-14 15:06:03 +0100

    1 file modified

--- script.js
+++ script.js
@@ -4,18 +4,7 @@ const changeBackgroundButton = document.getElementById(
   "changeBackgroundButton"
 );
 
-changeBackgroundButton.addEventListener("click", function() {
-  this.style.backgroundColor = getRandomColor();
-});
-
-// function getRandomColor() {
-//   const letters = getLetters();
-//   let color = '#';
-//   for (var i = 0; i < 6; i++) {
-//     color += letters[Math.floor(Math.random() * 16)];
-//   }
-//   return color;
-// }
+changeBackgroundButton.addEventListener("click", () => changeBackgroundButton.style.backgroundColor = getRandomColor());
 
 let getRandomColor = () => {
   let color = '#';
@@ -25,7 +14,4 @@ let getRandomColor = () => {
   return color;
 }
 
-// function getLetters(){
-//   return '0123456789ABCDEF';
-// }
 let getLetters = () => '0123456789ABCDEF';
